0ec87d87ce4b78af2a485ba813545cdffb203fbb
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-06-14 13:23:13 +0200

    feat: update project configuration and enhance UI components with styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChatComponent } from './components/chat/chat.component';
-import { HeaderComponent } from "./components/header/header.component";
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, ChatComponent, HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [ChatComponent, HeaderComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'my-calendar-frontend';
+    title = 'my-calendar-frontend';
 }
