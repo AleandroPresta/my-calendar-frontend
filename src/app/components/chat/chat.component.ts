0b8f6cb7cd201d169cfe70db4fe8e7566d3bb6d7
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-06-14 14:57:10 +0200

    feat: implement chat list and message components with initial templates and styles

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ChatListComponent } from './chat-list/chat-list.component';
 
 @Component({
-  selector: 'app-chat',
-  imports: [],
-  templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+    selector: 'app-chat',
+    imports: [ChatListComponent],
+    templateUrl: './chat.component.html',
+    styleUrl: './chat.component.css',
 })
-export class ChatComponent {
-
-}
+export class ChatComponent {}
